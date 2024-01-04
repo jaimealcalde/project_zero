259cd81119365a28a8a0787b7d8f1701531d3076
jaimealcalde <jaime.alcalde.n@gmail.com>
2024-01-04 20:08:12 +0100

    added entrees basics

--- src/app/components/entrees/entrees.component.ts
+++ src/app/components/entrees/entrees.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-entrees',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./entrees.component.css']
 })
 export class EntreesComponent {
+  clean: boolean = true;
+  entrees: any;
+  api: string = "/entrees";
 
+  constructor(public entreesApi:ApiService) {}
+
+  ngOnInit(){
+    this.entreesApi.getApi(this.api).subscribe({
+      next: res => {this.entrees = res, console.log(this.entrees), this.clean = false},
+      error: err => {console.log(err)}
+    });
+  }
+
+  addProduct(ingredient: any, description: any) {
+    let add: object = {
+      ingredient: ingredient.value,
+      description:  description.value,
+      gluten: "false",
+      lactose: "false"
+    };
+
+    this.entreesApi.postApi(add, this.api).subscribe({
+      next: res => {
+        this.entrees.push(res.ingredient), console.log(res.ingredient)},
+      error: err => {console.log(err)}
+    });
+  }
 }
